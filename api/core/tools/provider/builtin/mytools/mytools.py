aa7994f3904631a5be2c51aa3c309ebb7d1e4d36
SATOH Kiyoshi <345236+stealthinu@users.noreply.github.com>
2025-01-08 23:33:31 +0900

    プラグインの組織名を変更

--- api/core/tools/provider/builtin/mytools/mytools.py
+++
@@ -1,6 +0,0 @@
-from core.tools.provider.builtin_tool_provider import BuiltinToolProviderController
-
-
-class MyToolsProvider(BuiltinToolProviderController):
-    def _validate_credentials(self, credentials: dict) -> None:
-        pass
